46223d04674cc831dbbbe2ff766cc917e637eb13
mkolisnyk <kolesnik.nickolay@gmail.com>
2013-10-20 14:29:21 +0100

    Sources were migrated from Sirius repository

---
+++ src/main/java/sirius/utils/retriever/CucumberReportingPlugin.java
@@ -0,0 +1,298 @@
+/**
+ * 
+ */
+package sirius.utils.retriever;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import net.masterthought.cucumber.ReportBuilder;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.doxia.sink.Sink;
+import org.apache.maven.doxia.siterenderer.Renderer;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
+
+/**
+ * @author Myk Kolisnyk
+ * @goal cucumber
+ * @phase site
+ */
+public class CucumberReportingPlugin extends AbstractMavenReport {
+    public final String fs      = System.getProperty("file.separator");
+
+    /**
+     * Directory where reports will go.
+     *
+     * @parameter expression="${project.reporting.outputDirectory}"
+     * @required
+     * @readonly
+     */
+    private String outputDirectory;
+ 
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+
+    /**
+     * @component
+     * @required
+     * @readonly
+     */
+    private Renderer siteRenderer;
+    
+    /**
+     * @parameter
+     * @required
+     * @readonly
+     */
+    private String jsonFile;
+    
+    /**
+     * @parameter
+     * @readonly
+     */
+    private boolean skippedFails = false;
+
+    /**
+     * @parameter
+     * @readonly
+     */
+    private boolean undefinedFails = false;
+    
+    /**
+     * @parameter
+     * @readonly
+     */
+    private boolean flashCharts = false;
+    
+    /**
+     * @parameter
+     * @readonly
+     */
+    private boolean runWithJenkins = false;
+    
+    private boolean artifactsEnabled = false;
+    
+    private boolean highCharts = false;
+    
+    /* (non-Javadoc)
+     * @see org.apache.maven.reporting.MavenReport#getDescription(java.util.Locale)
+     */
+    public String getDescription(Locale arg0) {
+        // TODO Auto-generated method stub
+        return "HTML formatted Cucumber test results report";
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.maven.reporting.MavenReport#getName(java.util.Locale)
+     */
+    public String getName(Locale arg0) {
+        // TODO Auto-generated method stub
+        return "Test Execution Report";
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.maven.reporting.MavenReport#getOutputName()
+     */
+    public String getOutputName() {
+        // TODO Auto-generated method stub
+        return "cucumber-test-report";
+    }
+
+    /**
+     * @return the jsonFile
+     */
+    public String getJsonFile() {
+        return jsonFile;
+    }
+
+    /**
+     * @param jsonFile the jsonFile to set
+     */
+    public void setJsonFile(String jsonFile) {
+        this.jsonFile = jsonFile;
+    }
+
+    /**
+     * @return the skippedFails
+     */
+    public boolean isSkippedFails() {
+        return skippedFails;
+    }
+
+    /**
+     * @param skippedFails the skippedFails to set
+     */
+    public void setSkippedFails(boolean skippedFails) {
+        this.skippedFails = skippedFails;
+    }
+
+    /**
+     * @return the undefinedFails
+     */
+    public boolean isUndefinedFails() {
+        return undefinedFails;
+    }
+
+    /**
+     * @param undefinedFails the undefinedFails to set
+     */
+    public void setUndefinedFails(boolean undefinedFails) {
+        this.undefinedFails = undefinedFails;
+    }
+
+    /**
+     * @return the flashCharts
+     */
+    public boolean isFlashCharts() {
+        return flashCharts;
+    }
+
+    /**
+     * @param flashCharts the flashCharts to set
+     */
+    public void setFlashCharts(boolean flashCharts) {
+        this.flashCharts = flashCharts;
+    }
+
+    /**
+     * @return the runWithJenkins
+     */
+    public boolean isRunWithJenkins() {
+        return runWithJenkins;
+    }
+
+    /**
+     * @param runWithJenkins the runWithJenkins to set
+     */
+    public void setRunWithJenkins(boolean runWithJenkins) {
+        this.runWithJenkins = runWithJenkins;
+    }
+
+    /**
+     * @param outputDirectory the outputDirectory to set
+     */
+    public void setOutputDirectory(String outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    /**
+     * @param project the project to set
+     */
+    public void setProject(MavenProject project) {
+        this.project = project;
+    }
+
+    /**
+     * @param siteRenderer the siteRenderer to set
+     */
+    public void setSiteRenderer(Renderer siteRenderer) {
+        this.siteRenderer = siteRenderer;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.maven.reporting.AbstractMavenReport#executeReport(java.util.Locale)
+     */
+    @Override
+    protected void executeReport(Locale arg0) throws MavenReportException {
+        File reportOutputDirectory = new File(outputDirectory + fs + "cucumber" );
+        List<String> jsonReportFiles = new ArrayList<String>();
+        jsonReportFiles.add(this.jsonFile);
+
+        ReportBuilder reportBuilder;
+        try {
+            reportBuilder = 
+                    new ReportBuilder(
+                            jsonReportFiles, 
+                            reportOutputDirectory, 
+                            "", 
+                            project.getVersion(), 
+                            project.getName(), 
+                            skippedFails, 
+                            undefinedFails, 
+                            flashCharts, 
+                            runWithJenkins, 
+                            artifactsEnabled, 
+                            "", 
+                            highCharts);            
+            reportBuilder.generateReports();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        Sink sink = getSink();
+        sink.head();
+        sink.title();
+        sink.text("Cucumber Test Results Report");
+        sink.title_();
+        sink.head_();
+     
+        sink.body();
+        sink.section1();
+        sink.sectionTitle1();
+        sink.text("Features Overview");
+        sink.sectionTitle1_();
+        sink.paragraph();
+        sink.text("Features overview report shows test results distributed by each specific feature with detailed information about tests included");
+        sink.link("cucumber/feature-overview.html");
+        sink.text("View Report");
+        sink.link_();
+        sink.paragraph_();
+        sink.section1_();
+        
+        sink.section1();
+        sink.sectionTitle1();
+        sink.text("Tags Overview");
+        sink.sectionTitle1_();
+        sink.paragraph();
+        sink.text("Tags overview report shows test results distributed by tag with detailed information about tests included");
+        sink.link("cucumber/tag-overview.html");
+        sink.text("View Report");
+        sink.link_();
+        sink.paragraph_();
+        sink.section1_();
+
+        sink.body_();
+        sink.flush();
+        sink.close();
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.maven.reporting.AbstractMavenReport#getOutputDirectory()
+     */
+    @Override
+    protected String getOutputDirectory() {
+        // TODO Auto-generated method stub
+        return outputDirectory;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.maven.reporting.AbstractMavenReport#getProject()
+     */
+    @Override
+    protected MavenProject getProject() {
+        // TODO Auto-generated method stub
+        return project;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.maven.reporting.AbstractMavenReport#getSiteRenderer()
+     */
+    @Override
+    protected Renderer getSiteRenderer() {
+        // TODO Auto-generated method stub
+        return siteRenderer;
+    }
+
+}
---
+++ src/main/java/sirius/utils/retriever/IssueGetPlugin.java
@@ -0,0 +1,144 @@
+/**
+ * File description: Generates various output based on issues information in the GitHub.
+ * At the moment it has options to get the output in a form of Cucumber features
+ * or GitHub Wiki page with traceability matrix. 
+ */
+package sirius.utils.retriever;
+
+import java.io.IOException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.*;
+
+/**
+ * Generates various output based on issues information in the GitHub.
+ * At the moment it has options to get the output in a form of Cucumber features
+ * or GitHub Wiki page with traceability matrix.
+ * @author Myk Kolisnyk
+ * @version
+ */
+@Mojo(name = "generate",defaultPhase=LifecyclePhase.GENERATE_SOURCES)
+public class IssueGetPlugin extends AbstractMojo {
+
+    /**
+     * The user name to connect to GitHub repository with
+     */
+    @Parameter(property = "issueget.user", defaultValue = "")
+    private String   userName       = "";
+
+    /**
+     * The password to connect to GitHub repository
+     */
+    @Parameter(property = "issueget.password", defaultValue = "")
+    private String   password       = "";
+
+    /**
+     * The GitHub repository name to retrieve issues for
+     */
+    @Parameter(property = "issueget.repository", defaultValue = "Sirius")
+    private String   repository     = "";
+
+    /**
+     * Identifies the output type to be produced. At the moment the following values are supported:
+     * <ul>
+     * <li> <b>trace</b> - the output is the traceability matrix in a GitHub Wiki markdown format
+     * <li> <b>mvn-trace</b> - the output is the traceability matrix in HTML format 
+     * <li> <b>cucumber</b> - the output is the feature files containing the issues description
+     * </ul>
+     */
+    @Parameter(property = "issueget.type", defaultValue = "trace")
+    private String   outputType     = "";
+
+    /**
+     * The semi-colon separated list of labels which can be used as filter while quering
+     * issues. If multiple values are specified the issues returned should have at least one 
+     * of the specified labels.
+     */
+    @Parameter(property = "issueget.groups", defaultValue = "Test")
+    private String groups;
+
+    /**
+     * The folder where output should be produced to
+     */
+    @Parameter(property = "issueget.output", defaultValue = ".")
+    private String   outputLocation = "";
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    /**
+     * @param repository the repository to set
+     */
+    public void setRepository(String repository) {
+        this.repository = repository;
+    }
+
+
+
+    /**
+     * @param outputType the outputType to set
+     */
+    public void setOutputType(String outputType) {
+        this.outputType = outputType;
+    }
+
+
+
+    /**
+     * @param groups the groups to set
+     */
+    public void setGroups(String groups) {
+        this.groups = groups;
+    }
+
+
+
+    /**
+     * @param outputLocation the outputLocation to set
+     */
+    public void setOutputLocation(String outputLocation) {
+        this.outputLocation = outputLocation;
+    }
+
+
+
+    /**
+     * Main runner method
+     */
+    public void execute() throws MojoExecutionException {
+        String[] args = {
+                Program.REPO_NAME,
+                this.repository,
+                Program.USER_NAME,
+                this.userName,
+                Program.USER_PASS,
+                this.password,
+                Program.GROUPS,
+                this.groups,
+                Program.OUTPUT_TYPE,
+                this.outputType,
+                Program.OUTPUT_LOCATION,
+                this.outputLocation
+        };
+        try {
+            Program.main(args);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
